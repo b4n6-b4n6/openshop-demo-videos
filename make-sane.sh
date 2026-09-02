fca14b0fb0baca472846bea2e94206fad1a0f3b6
b4n6-b4n6 <b4n6-b4n6@yogi.pro>
2026-09-02 16:14:24 +0700

    rev

--- make-sane.sh
+++
@@ -1,4 +0,0 @@
-
-ffmpeg -i demo-shop-owner.mp4 -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k demo-shop-owner.out.mp4
-
-ffmpeg -i demo-shop-customer.mp4 -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k demo-shop-customer.out.mp4
